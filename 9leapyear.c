72b9dd456472082cc77b6eaba115c07fba466aaa
Saliq Shafi Bhat <bhatsaliq@gmail.com>
2025-11-10 11:37:18 +0530

    Add files via upload

---
+++ 9leapyear.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int year;
+    printf("Enter a year: \n");
+    scanf("%d", &year);
+
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0)
+                printf("Leap Year");
+            else
+                printf("Not a Leap Year");
+        } else
+            printf("Leap Year");
+    } else
+        printf("Not a Leap Year");
+
+    return 0;
+}
